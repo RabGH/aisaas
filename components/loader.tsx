c6577d50a4db73741171c513e71aa7c8e65cc9a7
RabGh <rabiighais@gmail.com>
2023-09-06 00:18:46 +0300

    Fix is loading state

--- components/loader.tsx
+++ components/loader.tsx
@@ -4,7 +4,7 @@ const Loader = () => {
   return (
     <div className="h-full flex flex-col gap-y-4 items-center justify-center">
       <div className="w-12 h-12 relative animate-spin rounded-full">
-        <Image alt="loader" fill src="/loading.svg" />
+        <Image alt="loader" fill src="/loading.svg" className="rounded-full" />
       </div>
       <p className="text-sm">Companion is thinking...</p>
     </div>
