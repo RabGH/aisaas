70eb4916a3b4674edebc390e4487f7c1d8ddc1f0
RabGh <rabiighais@gmail.com>
2023-09-05 14:13:20 +0300

    Sidebar pathnames & active route

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { Montserrat } from "next/font/google";
 import {
   Code,
@@ -62,10 +63,11 @@ const routes = [
 ];
 
 const SideBar = () => {
+  const pathname = usePathname();
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
       <div className="px-3 py-2 flex-1">
-        <Link href="/dashboard" className="flex items-center pl-3 mg-14">
+        <Link href="/dashboard" className="flex items-center pl-3 mb-14">
           <div className="relative w-14 h-14 mr-2">
             <Image fill alt="logo" src="/logo.png" />
           </div>
@@ -76,8 +78,13 @@ const SideBar = () => {
         <div className="space-y-1">
           {routes.map((route) => (
             <Link
-              className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer 
-            hover:text-white hover:bg-white/10 rounded-lg transition"
+              className={cn(
+                `text-sm group flex p-3 w-full justify-start font-medium cursor-pointer 
+              hover:text-white hover:bg-white/10 rounded-lg transition`,
+                pathname === route.href
+                  ? "text-white bg-white/10"
+                  : "text-zinc-400"
+              )}
               href={route.href}
               key={route.href}
             >
