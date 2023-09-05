db32e7d1510998c12200570793c148c3fe23131e
RabGh <rabiighais@gmail.com>
2023-09-05 13:32:40 +0300

    Mobile Nav & sidebar

---
+++ components/mobile-sidebar.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { Menu } from "lucide-react";
+
+import { useMount } from "@/hooks/mount-hook";
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import Sidebar from "@/components/sidebar";
+
+const MobileSidebar = () => {
+  const isMounted = useMount();
+  if (!isMounted) return null;
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0">
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileSidebar;
