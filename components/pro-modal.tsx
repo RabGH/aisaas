709275dd6a9be8ee815f3c42c6def2ea934eafd0
RabGh <rabiighais@gmail.com>
2023-09-07 17:13:21 +0300

    Install dialog, change z index, add pro modal

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -1,7 +1,30 @@
 "use client";
 
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { useProModal } from "@/hooks/use-pro-modal";
+
 const ProModal = () => {
-  return <div>ProModal</div>;
+  const proModal = useProModal();
+
+  return (
+    <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle
+            className="flex justify-center items-center flex-col 
+          gap-y-4 pb-2"
+          >
+            Upgrade Companion
+          </DialogTitle>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
 };
 
 export default ProModal;
