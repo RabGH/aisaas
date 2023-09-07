c5c40a802101031e8eaa1b2da997a4764b631f42
RabGh <rabiighais@gmail.com>
2023-09-07 16:44:14 +0300

    Add free counter box & shadcn progress

--- components/free-counter.tsx
+++ components/free-counter.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useMount } from "@/hooks/mount-hook";
+import { Card, CardContent } from "@/components/ui/card";
+import { MAX_FREE_COUNTS } from "@/constants";
 
 interface FreeCounterProps {
   apiLimitCount: number;
@@ -9,7 +11,17 @@ interface FreeCounterProps {
 const FreeCounter = ({ apiLimitCount }: FreeCounterProps) => {
   const isMounted = useMount();
   if (!isMounted) return null;
-  return <div></div>;
+  return <div className="px-3">
+    <Card className="bg-white/10 border-0">
+        <CardContent className="py-6">
+            <div className="text-center text-sm text-white mb-4 space-y-2">
+                <p>
+                    {apiLimitCount} / {MAX_FREE_COUNTS} Free Generations
+                </p>
+            </div>
+        </CardContent>
+    </Card>
+  </div>;
 };
 
 export default FreeCounter;
