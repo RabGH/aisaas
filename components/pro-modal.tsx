ba509d151cc657dd682f25af06ced1ac4714e2f5
RabGh <rabiighais@gmail.com>
2023-09-07 17:33:01 +0300

    Description & footer upgrade button

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -3,10 +3,21 @@
 import {
   Dialog,
   DialogContent,
+  DialogDescription,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
 import { useProModal } from "@/hooks/use-pro-modal";
+import { Badge } from "@/components/ui/badge";
+import { Card } from "@/components/ui/card";
+import { Check, Zap } from "lucide-react";
+
+import { aiTools } from "@/constants";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+
+const tools = aiTools;
 
 const ProModal = () => {
   const proModal = useProModal();
@@ -19,9 +30,39 @@ const ProModal = () => {
             className="flex justify-center items-center flex-col 
           gap-y-4 pb-2"
           >
-            Upgrade Companion
+            <div className="flex items-center gap-x-2 font-bold py-1">
+              Upgrade Companion
+              <Badge variant="premium" className="uppercase text-sm py-1">
+                pro
+              </Badge>
+            </div>
           </DialogTitle>
+          <DialogDescription
+            className="text-center pt-2 space-y-2 text-zinc-900 
+          font-medium"
+          >
+            {tools.map((tool) => (
+              <Card
+                key={tool.label}
+                className="p-3 border-black/5 flex items-center justify-between"
+              >
+                <div className="flex items-center gap-x-4">
+                  <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
+                    <tool.icon className={cn("w-6 h-6", tool.color)} />
+                  </div>
+                  <div className="font-semibold text-sm">{tool.label}</div>
+                </div>
+                <Check className="text-primary w-5 h-5" />
+              </Card>
+            ))}
+          </DialogDescription>
         </DialogHeader>
+        <DialogFooter>
+          <Button size="lg" variant="premium" className="w-full">
+            Upgrade
+            <Zap className="w-4 h-4 ml-2 fill-white" />
+          </Button>
+        </DialogFooter>
       </DialogContent>
     </Dialog>
   );
