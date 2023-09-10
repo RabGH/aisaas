bc7d19d460cb6ff760df71ce51b99dd4c45d67ca
RabGh <rabiighais@gmail.com>
2023-09-10 15:35:47 +0300

    Stripe onclick button upgrade

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -1,5 +1,15 @@
 "use client";
 
+import axios from "axios";
+import { useState } from "react";
+
+import { useProModal } from "@/hooks/use-pro-modal";
+import { Check, Zap } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { aiTools } from "@/constants";
+import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
+import { Card } from "@/components/ui/card";
 import {
   Dialog,
   DialogContent,
@@ -8,19 +18,25 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
-import { useProModal } from "@/hooks/use-pro-modal";
-import { Badge } from "@/components/ui/badge";
-import { Card } from "@/components/ui/card";
-import { Check, Zap } from "lucide-react";
-
-import { aiTools } from "@/constants";
-import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
 
 const tools = aiTools;
 
 const ProModal = () => {
   const proModal = useProModal();
+  const [loading, setLoading] = useState(false);
+
+  const onSubscribe = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get("/api/stripe");
+
+      window.location.href = response.data.url;
+    } catch (error) {
+      console.log(error, "STRIPE_CLIENT_ERROR");
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
@@ -58,7 +74,12 @@ const ProModal = () => {
           </DialogDescription>
         </DialogHeader>
         <DialogFooter>
-          <Button size="lg" variant="premium" className="w-full">
+          <Button
+            onClick={onSubscribe}
+            size="lg"
+            variant="premium"
+            className="w-full"
+          >
             Upgrade
             <Zap className="w-4 h-4 ml-2 fill-white" />
           </Button>
